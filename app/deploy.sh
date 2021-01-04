23cfc3437cbb7d3c1d802867653f8b135b07ff6a
ichino <chinoknct@gmail.com>
2021-01-04 16:35:33 +0900

    [fix]circleciの設定修正

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID="sample"
-export AWS_SECRET_ACCESS_KEY="sample"
-export AWS_BUCKET_NAME="sample"
-export AWS_CLOUDFRONT="sample"
+# export AWS_ACCESS_KEY_ID="sample"
+# export AWS_SECRET_ACCESS_KEY="sample"
+# export AWS_BUCKET_NAME="sample"
+# export AWS_CLOUDFRONT="sample"
 
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 
