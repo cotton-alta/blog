fa0a8b300279896d6ca4bb7f2152ebfa3df2ed94
ichino <chinoknct@gmail.com>
2021-01-04 15:47:04 +0900

    [add]CircleCIの設定ファイルを追加

---
+++ app/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID="sample"
+export AWS_SECRET_ACCESS_KEY="sample"
+export AWS_BUCKET_NAME="sample"
+export AWS_CLOUDFRONT="sample"
+
+[ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
+
+[ ! -d "node_modules" ] && yarn install
+
+yarn generate
+yarn run gulp deploy
