0bffb3dd213caa962dd1cffc6ce6911737625b1f
cotton <chinoknct@gmail.com>
2021-01-09 10:55:22 +0900

    [add]paginationを追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <div v-for="article in articles">
+    <div v-for="article in view_item" :key="article.id">
       <ArticleCard
         :title="article.title"
         :date="article.date"
         :content="article.content"
       />
     </div>
+    <b-pagination
+      v-model="current_page"
+      :total-rows="rows"
+      :per-page="per_page"
+      align="center"
+    >
+    </b-pagination>
   </div>
 </template>
 
@@ -19,16 +26,32 @@ export default Vue.extend({
     ArticleCard
   },
   data() {
+    const res_articles = [
+      { title: "タイトル", date: "2021-01-01", content: "本文" },
+      { title: "タイトル", date: "2021-01-01", content: "本文" },
+      { title: "タイトル", date: "2021-01-01", content: "本文" },
+      { title: "タイトル", date: "2021-01-01", content: "本文" },
+    ];
+    const res_per_page = 10;
     return {
-      articles: [
-        { title: "タイトル", date: "2021-01-01", content: "本文" },
-        { title: "タイトル", date: "2021-01-01", content: "本文" },
-        { title: "タイトル", date: "2021-01-01", content: "本文" }
-      ]
+      articles: res_articles,
+      current_page: 1,
+      per_page: res_per_page,
+      view_item: res_articles.slice(0, res_per_page)
+    }
+  },
+  computed: {
+    rows() {
+      return this.articles.length
+    }
+  },
+  watch: {
+    current_page: function(val) {
+      this.view_item = this.articles.slice((val - 1) * this.per_page, val * this.per_page);
     }
   }
 });
 </script>
 
-<style>
+<style module>
 </style>
