b21ed79caa67d0c8034a0a46cb575e195644c2db
cotton <chinoknct@gmail.com>
2021-12-19 11:58:25 +0900

    Fix ci job

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# export AWS_ACCESS_KEY_ID="sample"
-# export AWS_SECRET_ACCESS_KEY="sample"
-# export AWS_BUCKET_NAME="sample"
-# export AWS_CLOUDFRONT="sample"
-
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 
 [ ! -d "node_modules" ] && yarn install
