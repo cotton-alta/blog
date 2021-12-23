968321561ae423b823dc20057f89e60147975b3c
cotton <chinoknct@gmail.com>
2021-12-23 22:07:45 +0900

    Delete force option of git push

--- app/deploy_gh_pages.sh
+++ app/deploy_gh_pages.sh
@@ -5,7 +5,7 @@ git config user.email $GITHUB_EMAIL
 
 cd ..
 git add app && git commit -m "subtree commit for github pages"
-git subtree push -f --prefix app origin gh-pages
+git subtree push --prefix app origin gh-pages
 cd app
 
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
