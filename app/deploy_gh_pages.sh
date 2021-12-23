a256b8e296c5c4f2a84d64f11b7fa6e6d95118e9
cotton <chinoknct@gmail.com>
2021-12-23 18:49:59 +0900

    Fix circleci config

--- app/deploy_gh_pages.sh
+++ app/deploy_gh_pages.sh
@@ -3,6 +3,11 @@
 git config user.name $GITHUB_NAME
 git config user.email $GITHUB_EMAIL
 
+cd ..
+git add app && git commit -m "subtree commit for github pages"
+git subtree push -f --prefix app origin gh-pages
+cd app
+
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 
 [ ! -d "node_modules" ] && yarn install
